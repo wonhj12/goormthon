8243cfaa7cd707565150934bd13d73f8370f0d8c
원하진 <wonhj12@naver.com>
2024-02-28 11:33:12 +0900

    kakaomap settings

--- goormthon/Views/KakaoMapWrapper.swift
+++ goormthon/Views/KakaoMapWrapper.swift
@@ -4,8 +4,8 @@ struct KakaoMapWrapper: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         let kakaoMap = KakaoMapView()
         kakaoMap.baseMapType = .standard
-        kakaoMap.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: 33.388048, longitude: 126.563165)), animated: true)
-        kakaoMap.setZoomLevel(1, animated: true)
+        kakaoMap.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: 33.4996213, longitude: 126.5311884)), animated: true) // 제주도 위경도
+        kakaoMap.setZoomLevel(8, animated: false) // 제주도 한눈에 보이도록 zoom level 설정
         
         return kakaoMap
     }
