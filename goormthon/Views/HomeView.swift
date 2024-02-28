9579adc1658b9d4ea3d09d23699d2cd2b492dc37
dasoya <66422476+dasoya@users.noreply.github.com>
2024-02-28 23:26:43 +0900

    add home

---
+++ goormthon/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  goormthon
+//
+//  Created by dasoya on 2/28/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Image("")
+    }
+}
+
+#Preview {
+    HomeView()
+}
