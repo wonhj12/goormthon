88afc5207c5d6ce420e73236f73b8f904312540b
원하진 <wonhj12@naver.com>
2024-02-29 03:48:44 +0900

    update

--- goormthon/Extension.swift
+++ goormthon/Extension.swift
@@ -1,23 +1,29 @@
 //
-//  Extension.swift
+//  Extesion.swift
 //  goormthon
 //
-//  Created by dasoya on 2/28/24.
+//  Created by dasoya on 2/29/24.
 //
 
 import Foundation
-import SwiftUI
 
-extension Color{
-    
-    
-    static let mygray1 : Color = Color(red: 0.96, green: 0.96, blue: 0.98)
-    static let mygray2 : Color = Color(red: 0.91, green: 0.91, blue: 0.93)
-    static let mygray3 : Color = Color(red: 0.78, green: 0.78, blue: 0.78)
-    static let mygray4 : Color = Color(red: 0.56, green: 0.56, blue: 0.63)
-    static let mygray5 : Color = Color(red: 0.38, green: 0.38, blue: 0.45)
-    static let mygray6 : Color = Color(red: 0.27, green: 0.27, blue: 0.34)
-    static let myprimary : Color = Color(red: 1, green: 0.42, blue: 0.24)
-    static let mysecondary : Color = Color(red: 1, green: 0.57, blue: 0.44)
-    
+extension String {
+    func extractKeywords() -> [String] {
+        var keywords: [String] = []
+        
+        // 주어진 문자열에서 #으로 시작하는 문자열을 정규표현식을 사용하여 추출합니다.
+        let pattern = #"\#\w+"#
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: self, range: NSRange(location: 0, length: self.utf16.count))
+        
+        // 추출된 문자열을 배열에 추가합니다.
+        for match in matches {
+            if let range = Range(match.range, in: self) {
+                let keyword = String(self[range])
+                keywords.append(keyword)
+            }
+        }
+        
+        return keywords
+    }
 }
